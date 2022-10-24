df35b93b038050782d87696575840d21db14c314
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-10-24 16:41:19 +0200

    Update GraphLogic.cs

--- Assets/Scripts/Graph/GraphLogic.cs
+++ Assets/Scripts/Graph/GraphLogic.cs
@@ -13,6 +13,8 @@ namespace Graph
         public int numberOfNodes { get; private set; }
         public int numberOfEdges { get; private set; }
         public int numberOfEdgesPerNode { get; private set; }
+
+        public float graphBoundingCubeLength;
         // Start is called before the first frame update
         void Start()
         {
@@ -33,11 +35,46 @@ namespace Graph
 
             for (int i = 0; i < numberOfNodes; i++)
             {
-                Vector3 spawnPos = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
-                Nodes.Add( Instantiate(NodeObject, spawnPos, Quaternion.identity).GetComponent<Node>());
+                Vector3 spawnPos = RandPos();
+                Nodes.Add(Instantiate(NodeObject, spawnPos, Quaternion.identity).GetComponent<Node>());
+                Nodes[i].SetNumberOfConnectedNodes(edgesPerNode);
                 Nodes[i].position = spawnPos;
                 Nodes[i].timeToFinish = 1;
+                Nodes[i].NodeID = i;
             }
+            
+            Shuffle(Nodes);
+            for (int i = 1; i < numberOfNodes; i++)
+            {
+                CreateEdge(Nodes[i], Nodes[i-1]);
+            }
+        }
+
+        private Vector3 RandPos()
+        {
+            float bound = graphBoundingCubeLength * 0.5f;
+            Vector3 spawnPos = new Vector3(
+                Random.Range(-bound, bound),
+                Random.Range(-bound, bound),
+                Random.Range(-bound, bound)
+            );
+            return spawnPos;
+        }
+
+        /// <summary>
+        /// Shuffles the element order of the specified list.
+        /// </summary>
+        private static void Shuffle<T>(IList<T> nodeList) {
+            for (var i = 0; i < nodeList.Count - 1; ++i) {
+                var r = UnityEngine.Random.Range(i, nodeList.Count);
+                (nodeList[i], nodeList[r]) = (nodeList[r], nodeList[i]);
+            }
+        }
+
+        private void CreateEdge(Node a, Node b)
+        {
+            a.connectedNodes.Add(b);
+            b.connectedNodes.Add(a);
         }
     }
 }

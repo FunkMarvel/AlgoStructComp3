89444123c2c31df8366e91d22b7302d23fe67a8a
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-11-20 01:28:46 +0100

    Began implementing menu

---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string graphLevel = "Graph Scene";
+
+    public GameObject shortestPathScreen;
+    public GameObject travlingSalesmanScreen;
+
+    public TMP_InputField numNodesShort;
+    public InputField numEdgesShort;
+    public InputField numNodesTsp;
+
+    public int numberNodes = 20;
+    public int numberEdgesPerNode = 5;
+
+    public enum MenuState
+    {
+        MainMenuState,
+        ShortestPathState,
+        TravelingSalesmanState
+    }
+
+    public MenuState currentState = MenuState.MainMenuState;
+
+    private bool _bEnteringState = false;
+
+    public void OnChangeNumberNodes()
+    {
+        int fieldValue = 0;
+        int.TryParse(numNodesShort.text, out fieldValue);
+        
+        switch (currentState)
+        {
+            case MenuState.ShortestPathState:
+                if (numNodesShort)
+                {
+                    if (fieldValue < 2)
+                    {
+                        numberNodes = 2;
+                        numNodesShort.text = "2";
+                    }
+                    else if (fieldValue > 100)
+                    {
+                        numberNodes = 100;
+                        numNodesShort.text = "100";
+                    }
+                    else
+                    {
+                        numberNodes = fieldValue;
+                    }
+                }
+
+                break;
+            
+            case MenuState.TravelingSalesmanState:
+                if (numNodesTsp)
+                {
+                    if (fieldValue < 2)
+                    {
+                        numberNodes = 2;
+                        numNodesShort.text = "2";
+                    }
+                    else if (fieldValue > 25)
+                    {
+                        numberNodes = 25;
+                        numNodesShort.text = "25";
+                    }
+                    else
+                    {
+                        numberNodes = fieldValue;
+                    }
+                }
+
+                break;
+        }
+    }
+
+    public void OnChangeNumEdgesPerNode()
+    {
+        if (currentState == MenuState.TravelingSalesmanState && numEdgesShort)
+        {
+            int fieldValue = 0;
+            int.TryParse(numEdgesShort.text, out fieldValue);
+        }
+    }
+
+    public void ChangeState(MenuState newState)
+    {
+        if (currentState == newState)
+        {
+            _bEnteringState = false;
+            return;
+        }
+
+        if (currentState != newState)
+        {
+            _bEnteringState = true;
+            currentState = newState;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Awake()
+    {
+        if (shortestPathScreen) shortestPathScreen.SetActive(false);
+        if (travlingSalesmanScreen) travlingSalesmanScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void LoadGraph()
+    {
+        SceneManager.LoadScene(graphLevel);
+    }
+
+    public void OnShortestPath()
+    {
+        switch (currentState)
+        {
+            case MenuState.MainMenuState:
+                ChangeState(MenuState.ShortestPathState);
+                if (_bEnteringState)
+                {
+                    shortestPathScreen.SetActive(true);
+                    _bEnteringState = false;
+                }
+                break;
+            case MenuState.ShortestPathState:
+                break;
+        }
+    }
+
+    public void OnTravelingSalesman()
+    {
+        if (currentState == MenuState.MainMenuState)
+        {
+            ChangeState(MenuState.TravelingSalesmanState);
+            if (_bEnteringState)
+            {
+                travlingSalesmanScreen.SetActive(true);
+            }
+        }
+        
+        switch (currentState)
+        {
+            case MenuState.MainMenuState:
+                ChangeState(MenuState.TravelingSalesmanState);
+                if (_bEnteringState)
+                {
+                    travlingSalesmanScreen.SetActive(true);
+                    _bEnteringState = false;
+                }
+                break;
+            case MenuState.TravelingSalesmanState:
+                break;
+        }
+    }
+
+    public void OnBack()
+    {
+        switch (currentState)
+        {
+            case MenuState.ShortestPathState:
+                ChangeState(MenuState.MainMenuState);
+                shortestPathScreen.SetActive(false);
+                break;
+            case MenuState.TravelingSalesmanState:
+                ChangeState(MenuState.MainMenuState);
+                travlingSalesmanScreen.SetActive(false);
+                break;
+        }
+    }
+
+    public void OnExitGame()
+    {
+        if (currentState == MenuState.MainMenuState)
+        {
+            Application.Quit();
+            Debug.Log("Quitting");
+        }
+    }
+}
